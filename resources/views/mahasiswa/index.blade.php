24922d465637f049ac4319025047bf1b8044239d
luthfiazzz <luthfiazizi11@gmail.com>
2019-11-28 16:39:52 +0700

    crud done

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,46 @@
+@extends ('layout.main')    
+
+@section('title', 'About')
+
+@section('container')
+    <div class="container">
+    <div class="row">
+    <div class="col-10">
+    <h1 class="mt-3">Daftar Mahasiswa</h1>    
+    <!-- <a href="/students/create" class = "btn btn-primary my-3">Tambah Data Mahasiswa</a> -->
+    <div class="table">
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nama</th>
+      <th scope="col">NIM</th>
+      <th scope="col">Email</th>
+      <th scope="col">Jurusan</th>
+      <!-- <th scope="col">Aksi</th> -->
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($mahasiswa as $mhs)
+    <tr>
+      <th scope="row">{{$loop->iteration}}</th>
+      <td>{{$mhs->nama}}</td>
+      <td>{{$mhs->nim}}</td>
+      <td>{{$mhs->email}}</td>
+      <td>{{$mhs->jurusan}}</td>
+      <!-- <td>
+        <a href="" class="badge badge-success">Edit</a>
+        <a href="" class="badge badge-danger">Hapus</a>
+      </td> -->
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
+    </div>
+    </div>
+    </div>
+    </div>
+@endsection
+
+ 
